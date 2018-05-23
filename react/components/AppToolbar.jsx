42235765d0bceaa51515f0aca772cf3036277ae7
Benjamin Neff <benjamin@coding4coffee.ch>
2018-05-23 22:29:42 +0200

    Add link to GitHub to header

--- react/components/AppToolbar.jsx
+++ react/components/AppToolbar.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+
+import RefreshIcon from '@material-ui/icons/Refresh';
+
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
 
 import { withStyles } from '@material-ui/core/styles';
+
+import { fetchContexts } from '../actions/contexts'
+import { fetchPods } from '../actions/pods'
+import { fetchPodDescribe } from '../actions/pod'
+import { fetchContainerInfo } from '../actions/containerInfo'
 
 import ContextDropdown from './toolbar/ContextDropdown';
 import NamespaceDropdown from './toolbar/NamespaceDropdown';
 import PodDropdown from './toolbar/PodDropdown';
 import ContainerDropdown from './toolbar/ContainerDropdown';
 import PageDropdown from './toolbar/PageDropdown';
-import Refresh from './toolbar/Refresh';
 
 const styles = theme => ({
   root: {
@@ -33,25 +46,73 @@ const styles = theme => ({
   },
 });
 
-const AppToolbar = ({ classes }) => (
-  <AppBar position="sticky" className={classes.root}>
-    <Toolbar>
-      <Link to="/" className={classes.link}>
-        <img src="/static/images/favicon.png" className={classes.headerLogo} alt="k8s-logo" />
-        <Typography variant="title">
-          k8s deployments
-        </Typography>
-      </Link>
-      <div className={classes.headerMenu}>
-        <ContextDropdown />
-        <NamespaceDropdown />
-        <Route path="/:context/:namespace/:pod" component={PodDropdown} />
-        <Route path="/:context/:namespace/:pod/:container/:page" component={ContainerDropdown} />
-        <Route path="/:context/:namespace/:pod/:container/:page" component={PageDropdown} />
-      </div>
-      <Refresh />
-    </Toolbar>
-  </AppBar>
-);
+const mapStateToProps = (state, { match }) => ({
+  currentContext: match.params.context,
+  currentNamespace: match.params.namespace,
+  currentPod: match.params.pod,
+  currentContainer: match.params.container,
+  currentPage: match.params.page,
+});
 
-export default withStyles(styles)(AppToolbar);
+class AppToolbar extends Component {
+  handleRefresh = () => {
+    const { currentContext, currentNamespace, currentPod, currentContainer, currentPage } = this.props;
+
+    this.props.dispatch(fetchContexts());
+    this.props.dispatch(fetchPods(currentContext));
+
+    if (currentNamespace && currentPod) {
+      this.props.dispatch(fetchPodDescribe(currentContext, currentNamespace, currentPod));
+    }
+
+    if (currentNamespace && currentPod && currentContainer && currentPage) {
+      this.props.dispatch(fetchContainerInfo(currentContext, currentNamespace, currentPod, currentContainer, currentPage));
+    }
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <AppBar position="sticky" className={classes.root}>
+        <Toolbar>
+          <Link to="/" className={classes.link}>
+            <img src="/static/images/favicon.png" className={classes.headerLogo} alt="k8s-logo" />
+            <Typography variant="title">
+              k8s deployments
+            </Typography>
+          </Link>
+          <div className={classes.headerMenu}>
+            <ContextDropdown />
+            <NamespaceDropdown />
+            <Route path="/:context/:namespace/:pod" component={PodDropdown} />
+            <Route path="/:context/:namespace/:pod/:container/:page" component={ContainerDropdown} />
+            <Route path="/:context/:namespace/:pod/:container/:page" component={PageDropdown} />
+          </div>
+
+          <Tooltip id="appbar-refresh" title="Refresh Data" enterDelay={300}>
+            <IconButton
+              aria-labelledby="appbar-refresh"
+              onClick={this.handleRefresh}
+            >
+              <RefreshIcon />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip id="appbar-github" title="GitHub Repo" enterDelay={300}>
+            <IconButton
+              component="a"
+              href="https://github.com/SuperTux88/k8s-info"
+              target="_blank"
+              aria-labelledby="appbar-github"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(withStyles(styles)(AppToolbar)));
